02f612368853cc677e5066df490beb8fc28b1dd0
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2022-06-03 16:16:10 -0300

    feat: create simple players list with a mock

---
+++ src/components/header/header-tab.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import useQueryParams from "../../hooks/use-query-params";
+import Clickable from "../clickable";
+export type HeaderTabProps = {
+  label: string;
+  onClick: () => void;
+};
+
+const HeaderTab = ({ label, onClick }: HeaderTabProps) => {
+  const [isActive, setIsActive] = React.useState(false);
+  const { params } = useQueryParams();
+
+  React.useEffect(() => {
+    params.tab?.includes(label.toLocaleLowerCase())
+      ? setIsActive(true)
+      : setIsActive(false);
+  });
+
+  return (
+    <Clickable
+      onClick={onClick}
+      className={`transition-all duration-100 ${baseClasses} ${
+        isActive && isActiveClasses
+      }`}
+    >
+      <span className="text-sm font-thin">{label}</span>
+    </Clickable>
+  );
+};
+
+export default HeaderTab;
+
+const baseClasses = "flex hover:bg-gray-700 items-center px-12 py-2 text-white";
+const isActiveClasses = "bg-gray-900 border-b-4 border-blue-500";

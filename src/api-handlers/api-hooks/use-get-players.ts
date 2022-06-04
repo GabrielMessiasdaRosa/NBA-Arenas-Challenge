39205ec12ecc306ab91e06a652644c6f2164ce47
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2022-06-04 08:09:51 -0300

    refact: remove wrong fn

--- src/api-handlers/api-hooks/use-get-players.ts
+++ src/api-handlers/api-hooks/use-get-players.ts
@@ -1,4 +1,3 @@
-import { InferGetServerSidePropsType } from "next";
 import { useQuery } from "react-query";
 import getPlayersService from "../services/get-players-service";
 
@@ -8,34 +7,16 @@ export type useGetPlayersProps = {
     per_page: number;
     search?: string;
   };
-  players: InferGetServerSidePropsType<typeof getStaticProps>;
 };
 
-const useGetPlayers = ({ params, players }: useGetPlayersProps) => {
+const useGetPlayers = (params: useGetPlayersProps) => {
   const { data, error, status } = useQuery([`/players`, params], () =>
-    getPlayersService({ params })
+    getPlayersService(params)
   );
-  console.log("pageProps", players);
   return {
     data,
     error,
     pending: status === "loading",
   };
 };
-// This function gets called at build time on server-side.
-// It won't be called on client-side, so you can even do
-// direct database queries.
-export async function getStaticProps({ params }) {
-  // Call an external API endpoint to get posts.
-  // You can use any data fetching library
-  const players = await getPlayersService({ params });
-
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
-  return {
-    props: {
-      players,
-    },
-  };
-}
 export default useGetPlayers;
